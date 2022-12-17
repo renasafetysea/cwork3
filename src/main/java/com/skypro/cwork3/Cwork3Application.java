bdde17988d9eb153bbe39700f81cc1f6fe8457a4
Anna Molodtsova <renasafetysea@mail.ru>
2022-12-17 18:38:57 +0300

    first commit

---
+++ src/main/java/com/skypro/cwork3/Cwork3Application.java
@@ -0,0 +1,13 @@
+package com.skypro.cwork3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cwork3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cwork3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/cwork3/Cwork3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.cwork3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cwork3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
